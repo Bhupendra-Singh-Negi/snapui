81d49c110ebf7be2b8568b76ec74f877eef6d97b
Bhupendra Singh Negi <bhupendrasinghnegi11@gmail.com>
2026-04-28 16:53:13 +0530

    04_Header

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api(.*)",
-])
+  '/',
+  '/sign-in(.*)',
+  '/sign-up(.*)',
+  '/api(.*)',
+]);
 
-export default clerkMiddleware(async(auth , req)=>{
-  if(!isPublicRoute(req)){
-    await auth.protect()
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
 });
 
